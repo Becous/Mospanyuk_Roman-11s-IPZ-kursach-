592d6661a30185621a72f7f99e7a82af237880d4
Becous <becousgame@gmail.com>
2023-12-25 17:59:52 +0200

    меню

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,23 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const burgerMenuBtn = document.getElementById("burger-menu-btn");
-  const burgerMenu = document.getElementById("burger-menu");
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menuItem");
+const hamburger = document.querySelector(".hamburger");
+const closeIcon = document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
 
-  burgerMenuBtn.addEventListener("click", function () {
-    if (burgerMenu.style.display === "block") {
-      burgerMenu.style.display = "none";
-    } else {
-      burgerMenu.style.display = "block";
-    }
-  });
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+}
+
+hamburger.addEventListener("click", toggleMenu);
+
+menuItems.forEach(function (menuItem) {
+  menuItem.addEventListener("click", toggleMenu);
 });
