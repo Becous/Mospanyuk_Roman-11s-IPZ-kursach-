32266321197258ef3b2f4abd0849e1e5a02d5d6f
Becous <becousgame@gmail.com>
2023-12-25 18:16:49 +0200

    меню 'дороблене')

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,5 @@
 const menu = document.querySelector(".menu");
+const body = document.body;
 const menuItems = document.querySelectorAll(".menuItem");
 const hamburger = document.querySelector(".hamburger");
 const closeIcon = document.querySelector(".closeIcon");
@@ -6,12 +7,16 @@ const menuIcon = document.querySelector(".menuIcon");
 
 function toggleMenu() {
   if (menu.classList.contains("showMenu")) {
+    body.classList.remove("no-scroll");
     menu.classList.remove("showMenu");
     closeIcon.style.display = "none";
     menuIcon.style.display = "block";
   } else {
     menu.classList.add("showMenu");
+    body.classList.add("no-scroll");
     closeIcon.style.display = "block";
+    closeIcon.style.position = "fixed";
+    closeIcon.style.right = "0";
     menuIcon.style.display = "none";
   }
 }
