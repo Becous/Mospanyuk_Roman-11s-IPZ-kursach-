80b56feb37205012233c1c7da15aba75629aca6b
Becous <becousgame@gmail.com>
2024-01-03 17:21:21 +0200

    забув коєшо

--- js/swipers.js
+++ js/swipers.js
@@ -1,19 +1,19 @@
-var swiper_gal = new Swiper(".testimonial__swiper", {
-  slidesPerView: 'auto',
+var swiper_testimon = new Swiper(".testimonial__swiper", {
+  slidesPerView: "auto",
   spaceBetween: 100,
-  centeredSlides:true,
-  loop:true,
+  centeredSlides: true,
+  loop: true,
   pagination: {
     el: ".testimonial__pagination",
     clickable: true,
   },
 });
 
-var swiper_testim = new Swiper(".gallery__swpiper", {
-  slidesPerView: 'auto',
+var swiper_gal = new Swiper(".gallery__swpiper", {
+  slidesPerView: "auto",
   spaceBetween: 100,
-  centeredSlides:true,
-  loop:true,
+  centeredSlides: true,
+  loop: true,
   pagination: {
     el: ".gallery__pagination",
     clickable: true,
